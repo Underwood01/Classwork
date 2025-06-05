3d329d765038c18670e358259c70039b075739b3
Underwood01 <astrolight001@gmail.com>
2025-06-05 15:07:54 +0300

    Add files via upload

---
+++ classwork2,1/Car_rental_system.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class LoginSystem {
+
+    public static void main(String[] args) {
+
+        final String USERNAME = "admin";
+
+        final String PASSWORD = "1234";
+
+        Scanner scanner = new Scanner(System.in);
+
+        int attempts = 0;
+
+        boolean success = false;
+
+        // Loop for 3 attempts
+
+        while (attempts < 3) {
+
+            System.out.print("Enter username: ");
+
+            String inputUser = scanner.nextLine();
+
+            System.out.print("Enter password: ");
+
+            String inputPass = "";
+
+            // Show each char as typed
+
+            for (int i = 0; i < PASSWORD.length(); i++) {
+
+                char ch = scanner.next().charAt(0);
+
+                inputPass += ch;
+
+                System.out.print(ch); // print char
+
+            }
+
+            scanner.nextLine(); // clear buffer
+
+            System.out.println();
+
+            // Check credentials
+
+            if (inputUser.equals(USERNAME) && inputPass.equals(PASSWORD)) {
+
+                System.out.println("Login successful.");
+
+                success = true;
+
+                break;
+
+            } else {
+
+                System.out.println("Invalid login.");
+
+                attempts++;
+
+            }
+
+        }
+
+        if (!success) {
+
+            System.out.println("Access blocked after 3 attempts.");
+
+         }
+
+        scanner.close();
+
+    }
+
+}
